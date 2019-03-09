e872ff4f6f09e2d7197892c9fc8ac5332de544f5
fodil <fredfawkes96@gmail.com>
2019-03-09 23:44:21 +0100

    Zatchi back mdr

--- public/js/affectation.js
+++ public/js/affectation.js
@@ -6,14 +6,22 @@ $("#formModuleAtt").on('submit',function(e) {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-    var nombreG = $('#nombreG').val();
+    var nombreG = parseInt($('#nombreG').val());
+    var targetId = parseInt( $('#targetId').val());
+    var nombreEns =parseInt($('#nombreEns').val());
     var listTd = new Array();
     var listTp = new Array();
     var listeG = new Array();
+    var td = new Array();
+    var tp =new Array();
     for (var i=1;i<=nombreG;i++){
-        listTd.push(isChecked($('#td'+i)));
-        listTp.push(isChecked($('#tp'+i)));
-        listeG.push(parseInt($('#tp'+i).attr('role')));
+        listeG.push(parseInt($('#tp'+i).attr('target')));
+        td.push($("input[role='td"+i+"']"));
+        tp.push($("input[role='tp"+i+"']"));
+    }
+    for(var i=0;i<nombreG;i++){
+        listTd.push(isChecked(td[i][nombreEns-targetId]));
+        listTp.push(isChecked(tp[i][nombreEns-targetId]));
     }
     var moduleId = parseInt($('#moduleId').val());
     $.ajax({
@@ -34,10 +42,12 @@ $("#formModuleAtt").on('submit',function(e) {
 });
 $('.attaAction').on('click',function(){
     $('#ensIdInput').val($(this).attr('role'));
-});
+    $('#targetId').val($(this).attr('target'));
 
+});
+//$("input[role='td"+1+"']")
 function isChecked(val) {
-    if (val[0].checked)
+    if (val.checked)
         return 1;
     else return 0;
 }
